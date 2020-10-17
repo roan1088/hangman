7a8fa51a70bc28adff48da7c2bfb902c2cdefc90
roan1088 <rohan_188@yahoo.com>
2020-10-16 20:38:28 -0400

    Added disabling buttons once clicked

--- script.js
+++ script.js
@@ -15,9 +15,18 @@ $.each(alpahbetList, function(index, alphabet) {
 // When an alphabet button is clicked
 $(".alphabet-button").click(function() {
     console.log($(this).val());
+    // Disable the button
+    $(this).attr("disabled", true);
 });
 
 // When a key is pressed
 $(document).keydown(function(event) {
-    console.log(event.key.toUpperCase());
+    // Find the button of the letter pressed
+    var letterButton = $(".alphabet-button[value='" + event.key.toUpperCase() + "']")
+    // console.log(letterButton);
+    // Check if the button is enabled
+    if (!letterButton.attr("disabled")) {
+        // Trigger the click function of the button
+        letterButton.click();
+    }    
 });
