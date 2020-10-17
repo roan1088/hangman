fe5a4ba84a2bbfde251aed26d993e5ea6625077f
roan1088 <rohan_188@yahoo.com>
2020-10-17 14:55:16 -0400

    Now generates a random pokemon name

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 // Alphabets
 var alpahbetList = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+// List of pokemons (empty) to be filled by retrieving from API
+var pokemonList = [];
+// Variable for words
+var wordToGuess, wordGuess;
+// Variables for lives and scores
+var userLives = 5, userScore;
 
 // Build button area
 // For each character in the alphabet
@@ -12,9 +18,33 @@ $.each(alpahbetList, function(index, alphabet) {
     $(".button-area").append(buttonEl);
 });
 
-// Function to start the round by enabling all buttons
+// Function to get a random pokemon from the list
+function getPokemon() {
+    var rngIndex = Math.floor(Math.random() * pokemonList.length);
+    return pokemonList[rngIndex].name.toUpperCase();
+}
+
+// Function to start the round
 function startRound() {
+    // Enable all the alphabet buttons
     $(".alphabet-button").attr("disabled", false);
+    // Get a random word to be guessed
+    wordToGuess = getPokemon();
+    console.log(wordToGuess);
+    // Create the word with blanks
+    wordGuess = "";
+    $.each(wordToGuess.split(""), function(index, letter) {
+        if (alpahbetList.includes(letter)) {
+            wordGuess += '_';
+        }
+        else {
+            wordGuess += letter;
+        }
+    });
+    console.log(wordGuess);
+    // Update the display
+    $("#category").text("Genereation One Pokemon");
+    $("#word-view").text(wordGuess);
 }
 
 // When an alphabet button is clicked
@@ -36,4 +66,14 @@ $(document).keydown(function(event) {
     }    
 });
 
-startRound();
+// Game flow starts here
+// Request list of gen 1 pokemons
+$.ajax({
+    url: "https://pokeapi.co/api/v2/pokemon?limit=151",
+    method: "GET"
+}).then(function(response) {
+    // Store the retrieved pokemons
+    pokemonList = response.results;
+    // console.log(pokemonList);
+    startRound();
+});
